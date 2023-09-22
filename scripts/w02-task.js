72b1e7dfad855b7d91605ff19846a1ec7cb02a2a
daddymac77 <cri22004@byui.edu>
2023-09-22 22:45:09 +0800

    week 2 assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,29 +1,43 @@
-/* W02-Task - Profile Home Page */
+/* Lesson 2 */
 
-/* Step 1 - Setup type tasks - no code required */
+/* VARIABLES */
 
-/* Step 2 - Variables */
+// Step 1: declare and instantiate a variable to hold your name
+let fullName = "Mc-Coy Crisanto";
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector());
+const nameElement = document.getElementById("name");
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
+// Step 3: declare and instantiate a variable to hold the current year
+const currentYear = new Date().getFullYear();
+// Step 4: place the value of the current year variable into the HTML file
+document.querySelector("#year").innerText = currentYear;
+// Step 5: declare and instantiate a variable to hold the name of your picture
+const myPicture = "images/mypics.jpg";
+// Step 6: copy your image into the "images" folder
 
-
-
-/* Step 3 - Element Variables */
-
-
-
-
-
-/* Step 4 - Adding Content */
-
-
-
-
-
-
-/* Step 5 - Array */
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+document.querySelector("img").setAttribute("src", myPicture);
 
 
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+const favoriteFoods = ["Chorizo", "Tokwa", "Lettuce", "Bread"]
 
 
+// Step 2: place the values of the favorite foods variable into the HTML file
+// Retrieve the foodElement
+// Retrieve the foodElement
+const foodElement = document.getElementById("food");
+
+// Declare and instantiate a variable for the new favorite food
+const newFavoriteFood = ["rice", "cheese", "peanut butter"]; // Replace with your favorite food item
+
+// Add the new favorite food to the favoriteFoods array
+favoriteFoods.push(newFavoriteFood);
+
+// Update the foodElement to display the updated favorite foods with line breaks
+foodElement.innerHTML = favoriteFoods.join("<br>");
 
 
